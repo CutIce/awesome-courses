119d5e935af4c7a647b126783202d75a462d8387
CutIce <1031941295@qq.com>
2021-02-16 13:48:37 +0800

    JSON Tutorial : finish tutorial 6

--- json-tutorial-master/tutorial06/leptjson.c
+++ json-tutorial-master/tutorial06/leptjson.c
@@ -127,18 +127,19 @@ static void lept_encode_utf8(lept_context* c, unsigned u) {
 
 #define STRING_ERROR(ret) do { c->top = head; return ret; } while(0)
 
-static int lept_parse_string(lept_context* c, lept_value* v) {
-    size_t head = c->top, len;
-    unsigned u, u2;
+static int lept_parse_string_raw(lept_context* c, char ** str, size_t* len) {
+    size_t head = c->top;
+    unsigned u,u2;
     const char* p;
     EXPECT(c, '\"');
     p = c->json;
+
     for (;;) {
         char ch = *p++;
         switch (ch) {
             case '\"':
-                len = c->top - head;
-                lept_set_string(v, (const char*)lept_context_pop(c, len), len);
+                *len = c->top - head;
+                *str = lept_context_pop(c, *len);
                 c->json = p;
                 return LEPT_PARSE_OK;
             case '\\':
@@ -179,6 +180,17 @@ static int lept_parse_string(lept_context* c, lept_value* v) {
                 PUTC(c, ch);
         }
     }
+}
+
+
+static int lept_parse_string(lept_context* c, lept_value* v) {
+    int ret;
+    char *s;
+    size_t len;
+
+    if ((ret = lept_parse_string_raw(c, &s, &len)) == LEPT_PARSE_OK)
+        lept_set_string(v, s, len);
+    return ret;
 }
 
 static int lept_parse_value(lept_context* c, lept_value* v);
@@ -242,9 +254,27 @@ static int lept_parse_object(lept_context* c, lept_value* v) {
     m.k = NULL;
     size = 0;
     for (;;) {
+        char *str;
         lept_init(&m.v);
         /* \todo parse key to m.k, m.klen */
+        if (*c->json != '"') {
+            ret = LEPT_PARSE_MISS_KEY;
+            break;
+        }
+        if ((ret = lept_parse_string_raw(c, &str, &m.klen)) != LEPT_PARSE_OK)
+            break;
+        memcpy(m.k = (char *) malloc(m.klen + 1), str, m.klen);
+        m.k[m.klen] = '\0';
+
         /* \todo parse ws colon ws */
+        lept_parse_whitespace(c);
+        if (*c->json != ':') {
+            ret = LEPT_PARSE_MISS_COLON;
+            break;
+        }
+        c->json++;
+        lept_parse_whitespace(c);
+
         /* parse value */
         if ((ret = lept_parse_value(c, &m.v)) != LEPT_PARSE_OK)
             break;
@@ -252,8 +282,36 @@ static int lept_parse_object(lept_context* c, lept_value* v) {
         size++;
         m.k = NULL; /* ownership is transferred to member on stack */
         /* \todo parse ws [comma | right-curly-brace] ws */
+        lept_parse_whitespace(c);
+        /*
+         * if (*c->json == ',') c->json++;
+        lept_parse_whitespace(c);
+         */
+        /* better than me*/
+        if (*c->json == ',') {
+            c->json++;
+            lept_parse_whitespace(c);
+        } else if (*c->json == '}') {
+            c->json++;
+            size_t s = sizeof(lept_member) * size;
+            v->type = LEPT_OBJECT;
+            v->u.o.size = size;
+            memcpy(v->u.o.m = malloc(s), lept_context_pop(c, s), s);
+            return LEPT_PARSE_OK;
+        } else {
+            ret = LEPT_PARSE_MISS_COMMA_OR_CURLY_BRACKET;
+            break;
+        }
     }
     /* \todo Pop and free members on the stack */
+    int i;
+    free(m.k);
+    for (i = 0; i < size; ++i) {
+        lept_member * m = (lept_member*) lept_context_pop(c, sizeof(lept_member));
+        free(m->k);
+        lept_free(&m->v);
+    }
+    v->type = LEPT_NULL;
     return ret;
 }
 
--- json-tutorial-master/tutorial06/test.c
+++ json-tutorial-master/tutorial06/test.c
@@ -352,7 +352,7 @@ static void test_parse() {
     test_parse_number();
     test_parse_string();
     test_parse_array();
-#if 0
+#if 1
     test_parse_object();
 #endif
 
@@ -366,7 +366,7 @@ static void test_parse() {
     test_parse_invalid_unicode_hex();
     test_parse_invalid_unicode_surrogate();
     test_parse_miss_comma_or_square_bracket();
-#if 0
+#if 1
     test_parse_miss_key();
     test_parse_miss_colon();
     test_parse_miss_comma_or_curly_bracket();
